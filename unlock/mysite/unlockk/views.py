8319970b7f140d1eed5923b653539c3b3d85e044
yyyyd1008 <hanyedam65@gmail.com>
2024-03-31 23:18:54 +0900

    친구 추가, 삭제 구현중 (팔로우 기능 응용하면 될듯)

--- unlock/mysite/unlockk/views.py
+++ unlock/mysite/unlockk/views.py
@@ -24,7 +24,6 @@ def home(request):
     return render(request, 'unlockk/home.html')
 
 def follow(request):
-    users = User.objects.all()  # 모든 사용자를 가져옴
     kw = request.GET.get('kw', '')  # 검색어
     user_list = []  # 검색 결과를 담을 리스트 초기화
 
@@ -32,39 +31,36 @@ def follow(request):
         # 검색어를 포함한 사용자를 필터링하여 리스트에 추가
         user_list = User.objects.filter(Q(username__icontains=kw))
 
-    context = {'users':users, 'kw': kw, 'user_list': user_list}
+    context = {'kw': kw, 'user_list': user_list}
     return render(request, 'unlockk/follow.html', context)
 
 
 # 팔로우 목록
-@login_required(login_url='common:login')
-def follow_list(request,user_id):
-    follow = get_object_or_404(Follow,user_id=user_id)
-    userList = User.objects.exclude(pk=request.user.pk)     #자기자신을 제외하고 불러옴
+@login_required(login_url='common:signin')
+def follow_list(request, user_id):
+    follow = get_object_or_404(Follow, user_id = user_id)
+    users = User.objects.exclude(pk = request.user.pk)     # 자기자신을 제외하고 불러옴
     
-    context = {'follow' : follow , 'userList' : userList}
-    
-    return render(request,'unlockk/follow.html',context)
-    
+    context = {'follow': follow , 'users': users}
+    return render(request, 'unlockk/follow.html', context)
     
     
-#팔로우
-@login_required(login_url='common:login')
-def following(request,to_user_id):
-    to_user = get_object_or_404(User,pk = to_user_id)
-    followUser = get_object_or_404(Follow,user = request.user)
+# 팔로우
+@login_required(login_url='common:signin')
+def following(request, to_user_id):
+    to_user = get_object_or_404(User, pk = to_user_id)
+    followUser = get_object_or_404(Follow, user = request.user)
     
     followUser.to_user.add(to_user)
+
+    return redirect('unlockk:follow_list', user_id = request.user.id)
     
-    
-    return redirect('unlockk:follow_list',user_id = request.user.id)
-    
-#언팔로우
-@login_required(login_url='common:login')
-def unFollowing(request,to_user_id):
-    to_user = get_object_or_404(User,pk = to_user_id)
-    followUser = get_object_or_404(Follow,user = request.user)
+# 언팔로우
+@login_required(login_url='common:signin')
+def unFollowing(request, to_user_id):
+    to_user = get_object_or_404(User, pk = to_user_id)
+    followUser = get_object_or_404(Follow, user = request.user)
     
     followUser.to_user.remove(to_user)
     
-    return redirect('unlockk:follow_list',user_id = request.user.id)
+    return redirect('unlockk:follow_list', user_id = request.user.id)
